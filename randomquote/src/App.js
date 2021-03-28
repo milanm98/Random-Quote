a694602508f4b5d62dc34be0d6b23f548de9c082
milanm98 <marjanovmilan13@gmail.com>
2021-03-28 14:56:36 -0400

    start

--- randomquote/src/App.js
+++ randomquote/src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [quotes, setQuotes] = useState([""]);
+  const [authors, setAuthors] = useState([""]);
+
   return (
-    <div className="App">
-     
+    <div className="w-screen h-screen bg-red-100 text-red-500">
+       alo
     </div>
   );
 }
